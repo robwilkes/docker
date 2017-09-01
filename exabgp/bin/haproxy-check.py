d707f0e97f1a574e642ea5f0bc844f2da2d3f534
Rob Wilkes <mail@robbiew.net>
2017-09-01 13:57:51 +1000

    added healthcheck for frontend in addition to backend

--- exabgp/bin/haproxy-check.py
+++ exabgp/bin/haproxy-check.py
@@ -6,15 +6,22 @@ URL = sys.argv[1]
 USER = sys.argv[2]
 PASS = sys.argv[3]
 MODE = sys.argv[4]
-BEND = sys.argv[5]
-VERIFY = False if sys.argv[6].lower() == 'false' else True
+VERIFY = False if sys.argv[5].lower() == 'verify_false' else True
+COMP = sys.argv[6]
+NAME = sys.argv[7]
 
 haproxy = HAProxyServer(URL,USER,PASS,MODE,VERIFY)
 
-for b in haproxy.backends:
-    if b.name == BEND and b.status == 'UP':
-        print('%s: %s' % (b.name, b.status))
-        sys.exit(0)
+if COMP == 'backend':
+    for b in haproxy.backends:
+        if b.name == NAME and b.status == 'UP':
+            print('%s: %s' % (b.name, b.status))
+            sys.exit(0)
+elif COMP == 'frontend':
+    for b in haproxy.frontends:
+        if b.name == NAME and b.status == 'OPEN':
+            print('%s: %s' % (b.name, b.status))
+            sys.exit(0)
 
 print('%s: %s' % (sys.argv[2], 'DOWN'))
 sys.exit(1)
